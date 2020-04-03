300e02482bdd46451e4723c303994db74f46a330
Jani Turunen <janisku7@gmail.com>
2020-04-04 00:38:35 +0300

    things for dashboard

---
+++ ServicesKomponentUnit.Data/ChannelComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using JT7SKU.Lib.Twitch;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace ServicesKomponentUnit.Data.Twitch
+{
+    public partial class ChannelComponent
+    {
+        [Parameter]
+        public string ChannelBoxUrl { get; set; }
+        [Parameter]
+        public string ChannelName { get; set; }
+        [Parameter]
+        public Color StatusColor { get; set; }
+        protected override Task OnParametersSetAsync()
+        {
+            return base.OnParametersSetAsync();
+        }
+    }
+}
---
+++ ServicesKomponentUnit.Data/Dashboard.razor.cs
@@ -0,0 +1,33 @@
+﻿using JT7SKU.Lib.Twitch;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace ServicesKomponentUnit.Data.Twitch
+{
+    public  class Dashboard
+    {
+        public int Count { get; set; }
+        [Parameter]
+        public IEnumerable<Channel> Channels { get; set; }
+        //protected override Task OnParametersSetAsync()
+        //{
+        //    return base.OnParametersSetAsync();
+        //}
+        //protected override Task OnInitializedAsync()
+        //{
+        //    Count = Channels.Count();
+        //    return base.OnInitializedAsync();
+        //}
+        Color ChannelStatusToColor(ChannelStatus status)
+        {
+            return System.Drawing.Color.White;
+        }
+    }
+}
