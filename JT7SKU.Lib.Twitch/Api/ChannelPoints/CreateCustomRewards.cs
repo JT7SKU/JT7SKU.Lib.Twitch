698a6008138115b76475a83704642a1654a845e9
Janisku7 <janisku7@gmail.com>
2024-03-07 02:53:51 +0200

    work continue

--- JT7SKU.Lib.Twitch/Api/ChannelPoints/CreateCustomRewards.cs
+++ JT7SKU.Lib.Twitch/Api/ChannelPoints/CreateCustomRewards.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using JT7SKU.Lib.Twitch.EventSub;
+using JT7SKU.Lib.Twitch.Models;
+using Orleans.Streams;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -28,6 +31,18 @@ namespace JT7SKU.Lib.Twitch.Api.ChannelPoints
         public string Title { get; set; }
         [Required]
         public Int64 Cost { get; set; }
+        [MaxLength(200)]
+        public string Prompt { get; set; }
+        public bool IsEnabled { get; set; }
+        public string BackgroundColor { get; set; }
+        public bool IsUserInputRequired { get; set; }
+        public bool IsMaxPerStreamEnabled { get; set; }
+        public int MaxPerStream { get; set; }
+        public bool IsMaxPerUserPerStreamEnabled { get; set; }
+        public int MaxPerUserPerStream { get; set; }
+        public bool IsGlobalCooldownEnabled { get; set; }
+        public int GlobalCooldowInSeconds { get; set; }
+        public bool ShouldRedemptionsSkipRequestQueue { get; set; }
     }
     public record CreateCustomRewardResponseBody 
     {
@@ -35,11 +50,34 @@ namespace JT7SKU.Lib.Twitch.Api.ChannelPoints
     }
     public record CreateCustomRewardData
     {
-
+        public string BroadcasterId { get; set; }
+        public string BroadcasterLogin { get; set; }
+        public string BroadcasterName { get; set; }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Prompt { get; set; }
+        public int Cost { get; set; }
+        public Image Image { get; set; }
+        public Image DefaultImage { get; set; }
+        public string BackgroundColor { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsUserInputEnabled { get; set; }
+        public MaxPerStream MaxPerStreamSetting { get; set; }
+        public MaxPerUserPerStream MaxPerUserPerStreamSetting { get; set; }
+        public GlobalCooldown GlobalCooldownSetting { get; set; }
+        public bool IsPaused { get; set; }
+        public bool IsInStock { get; set; }
+        public bool ShouldRedemptionsSkipRequestQueue { get; set; }
+        public int RedemptionsRedeemedCurrentStream { get; set; }
+        public string CooldownExpiresAt { get; set; }
     }
     public enum CreateCustomRewardResponseCodes
     {
-        OK=200
+        OK=200,
+        BadRequest=400,
+        Unauthorized=401,
+        Forbidden=403,
+        InternalServerError=500
     }
 
 }
--- JT7SKU.Lib.Twitch/Api/ChannelPoints/DeleteCustomReward.cs
+++ JT7SKU.Lib.Twitch/Api/ChannelPoints/DeleteCustomReward.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace JT7SKU.Lib.Twitch.Api.ChannelPoints
 {
     public class DeleteCustomReward
     {
+        public string DeleteCustomRewardScope { get; set; } = "channel:manage:redemptions";
+        public string ClientId { get; set; }
+        public string PathUrl { get; set; } = "/channel_points/custom_rewards";
+        public DeleteCustomRewardRequestQuery RequestQuery { get; set; }
+        public DeleteCustomRecordResponseCodes ResponseCodes { get; set; }
+    }
+    public record DeleteCustomRewardRequestQuery
+    {
+        [Required]
+        public string BroadcasterId { get; set; }
+        [Required]
+        public string Id { get; set; }
+    }
+    public enum DeleteCustomRecordResponseCodes
+    {
+        NoContent=204,
+        BadRequest=400,
+        Unauthorized=401,
+        Forbidden=403,
+        NotFound=404,
+        InternalServerError=500
     }
 }
