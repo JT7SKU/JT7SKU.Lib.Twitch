faed676d1103b1a1433f0199e0b6252b7c94bc3e
Janisku7 <janisku7@gmail.com>
2024-03-14 02:38:07 +0200

    update things

---
+++ Stream.Komponent.Unit.Extension.Twitch/VideoOverlay.razor.cs
@@ -0,0 +1,16 @@
+﻿using JT7SKU.Lib.Twitch.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream.Komponent.Utility.Extension.Twitch
+{
+    partial class VideoOverlay : ComponentBase
+    {
+        [Parameter]
+        public ExtensionVideoOverlay ExtensionVideoOverlay { get; set; }
+    }
+}
