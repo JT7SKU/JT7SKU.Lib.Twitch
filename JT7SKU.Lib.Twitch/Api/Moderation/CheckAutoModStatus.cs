f655a48e3b0c30fc3f44dbf6245dd414eb05496f
Janisku7 <janisku7@gmail.com>
2024-03-15 22:28:43 +0200

    small things

--- JT7SKU.Lib.Twitch/Api/Moderation/CheckAutoModStatus.cs
+++ JT7SKU.Lib.Twitch/Api/Moderation/CheckAutoModStatus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JT7SKU.Lib.Twitch.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace JT7SKU.Lib.Twitch.Api.Moderation
 {
     internal class CheckAutoModStatus
     {
+        public Ratelimit Ratelimit { get; set; }
+
+
     }
 }
---
+++ JT7SKU.Lib.Twitch/Utils/Ratelimit.cs
@@ -0,0 +1,9 @@
+﻿namespace JT7SKU.Lib.Twitch.Utils
+{
+    public class Ratelimit 
+    {
+        public AccountType AccountType { get; set; } 
+        public int LimitPerMin {  get; set; } 
+        public int LimitPerHour {get; set; } 
+    }
+}
