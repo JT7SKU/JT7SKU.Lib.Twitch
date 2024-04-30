055d1ffb02415c4c086b52bff025df46b900b004
Janisku7 <janisku7@gmail.com>
2024-04-30 14:48:38 +0300

    the thing started

--- JT7SKU.Lib.Twitch/EventSub/Subscriptions/Automod/Message/Hold.cs
+++ JT7SKU.Lib.Twitch/EventSub/Subscriptions/Automod/Message/Hold.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using JT7SKU.Lib.Twitch.EventSub.Conditions.Automod;
+using JT7SKU.Lib.Twitch.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JT7SKU.Lib.Twitch.EventSub.Subscriptions.Automod.Message
 {
+    // The automod.message.hold subscription type notifies a user if a message was caught by automod for review.
+    // equires a user access token that includes the moderator:manage:automod scope.
+    // The ID in the moderator_user_id condition parameter must match the user ID in the access token.
+    // If app access token used, then additionally requires the moderator:manage:automod scope for the moderator.
     public class Hold
     {
+    }
+    public record AutoModMessageHoldRequestBody
+    {
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Version { get; set; }
+        [Required]
+        public AutoModMessageHoldCondition Condition { get; set; }
+        [Required]
+        public Transport Transport { get; set; }
+    }
+    public record AutomodMessageHoldNotificationPayload
+    {
+        public Subscription Subscription { get; set; }
+        public Event Event { get; set; }
     }
 }
--- JT7SKU.Lib.Twitch/EventSub/Subscriptions/Automod/Message/Update.cs
+++ JT7SKU.Lib.Twitch/EventSub/Subscriptions/Automod/Message/Update.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using JT7SKU.Lib.Twitch.EventSub.Conditions.Automod;
+using JT7SKU.Lib.Twitch.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JT7SKU.Lib.Twitch.EventSub.Subscriptions.Automod.Message
 {
+    // The automod.message.update subscription type sends notification when a message in the automod queue has its status changed.
+    // Requires a user access token that includes the moderator:manage:automod scope.
+    // The ID in the moderator_user_id condition parameter must match the user ID in the access token.
+    // If app access token used, then additionally requires the moderator:manage:automod scope for the moderator.
+    // The moderator must be a moderator or broadcaster for the specified broadcaster.
     internal class Update
     {
+    }
+    public record AutomodMessageUpdate
+    {
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Version { get; set; }
+        [Required]
+        public AutomodMessageUpdateCondition Condition { get; set; }
+        [Required]
+        public Transport Transport { get; set; }
+    }
+    public record AutomodMessageUpdateNotificationPayload
+    {
+
+        public Subscription Subscription { get; set; }
+        public Event Event { get; set; }
     }
 }
