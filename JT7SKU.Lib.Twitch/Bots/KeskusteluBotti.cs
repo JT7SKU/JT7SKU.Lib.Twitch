c84c375830817d74278a7a82853a03b13a00b6ee
Janisku7 <janisku7@gmail.com>
2024-06-05 11:53:41 +0300

    small things updated the bts started

---
+++ JT7SKU.Lib.Twitch/Bots/KeskusteluBotti.cs
@@ -0,0 +1,19 @@
+﻿using JT7SKU.StreamService.KirjastoUtility.Twitch.Utils;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT7SKU.StreamService.KirjastoUtility.Twitch.Bots
+{
+    internal class KeskusteluBotti
+    {
+        public string BotName { get; set; }
+        public BotRatelimit BotRateLimit { get; set; }
+        public List<string> Kommands { get; set; }
+        public string Membership { get; set; }
+        public List<string> Tags {  get; set; } 
+    }
+}
---
+++ JT7SKU.Lib.Twitch/Utils/BotRatelimit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT7SKU.StreamService.KirjastoUtility.Twitch.Utils
+{
+    internal class BotRatelimit
+    {
+        
+    }
+}
