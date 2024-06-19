9c1ac55d7919148b70b4719f886b0e23694a58e7
Janisku7 <janisku7@gmail.com>
2024-06-19 12:18:12 +0300

    updated changes

--- JT7SKU.Lib.Twitch/Models/Chat/ChatMessageType.cs
+++ JT7SKU.Lib.Twitch/Models/Chat/ChatMessageType.cs
@@ -9,6 +9,6 @@ namespace JT7SKU.StreamService.KirjastoUtility.Twitch.Models
     public enum ChatMessageType
     {
         text, channel_points_highlighted, channel_points_sub_only,
-        user_intro, animated, gigantified_emote
+        user_intro, power_ups_message_effect, power_ups_gigantified_emote
     }
 }
